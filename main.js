d0152fa5b0a3c8243a910223434759040260531d
Noel Whitaker <noelw55@gmail.com>
2021-12-23 11:46:57 -0500

    feat(*): icons -> assets; customizable configs; draggable

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 const { app, BrowserWindow, screen } = require('electron');
+const fs = require('fs');
 
-// const backgroundOpacity = '7F'; //https://stackoverflow.com/questions/7015302/css-hexadecimal-rgba
-const backgroundOpacity = 'FF';
-const textDarkness = '100%';
-const sectionsToRemove = ['top', 'bottom'];
-const stylesToApply = [
-  {
-    id: 'centerContent',
-    key: 'display',
-    value: 'block'
-  },
-  {
-    id: 'words',
-    key: 'filter',
-    value: `brightness(${textDarkness})`
+const monkeytypeURL = 'https://monkeytype.com';
+
+// Opacity Options: //https://stackoverflow.com/questions/7015302/css-hexadecimal-rgba
+
+const noGridContentStyle = {
+  id: 'centerContent',
+  key: 'display',
+  value: 'block'
+};
+
+const wordBrightnessStyle = (textBrightness) => ({
+  id: 'words',
+  key: 'filter',
+  value: `brightness(${textBrightness})`
+});
+
+const getConfig = (path) => {
+  var configObj = {};
+  try {
+    configObj = JSON.parse(fs.readFileSync(path, {encoding: "utf8"}));
+  } catch (e) {
+    console.log('failed to parse config file');
+    console.log(e);
   }
-];
+  return configObj;
+}
 
 const removeElemById = (window) => (id) =>
   window.webContents.executeJavaScript(`
@@ -29,49 +40,82 @@ const applyStylesById = (window) => ({id, key, value}) =>
     ${id}StyleElem.style.${key} = '${value}';
   `);
 
+const setBackgroundOpacity = (window, backgroundOpacity) => {
+   window.webContents.executeJavaScript(`
+    const bgColor = getComputedStyle(document.body).getPropertyValue('--bg-color');
+    document.body.style.backgroundColor = bgColor + '${backgroundOpacity}';
+  `);
+}
+
+const makeKeyDownDraggableHandle = (window, elemID, keyCode) => {
+  window.webContents.executeJavaScript(`
+    const draggableElem = document.getElementById('${elemID}');
+    let keyDown = false;
+    document.addEventListener('keydown', (evt) => {
+      if(evt.keyCode === ${keyCode} && !keyDown) {
+        keyDown = true;
+        draggableElem.style['-webkit-user-select'] = 'none';
+        draggableElem.style['-webkit-app-region'] = 'drag';
+      }
+    });
+    document.addEventListener('keyup', (evt) => {
+      if(evt.keyCode === ${keyCode} && keyDown) {
+        keyDown = false;
+        draggableElem.style['-webkit-user-select'] = '';
+        draggableElem.style['-webkit-app-region'] = '';
+      }
+    });
+  `);
+}
+
+const config = getConfig('./config.json');
+console.log(config);
+const sectionsToRemove = config.idsToRemove || [];
+let stylesToApply = [];
+if (sectionsToRemove.includes('top') && sectionsToRemove.includes('bottom')) {
+  // Removes the grid style to remove the scrollbar
+  stylesToApply = [...stylesToApply, noGridContentStyle]
+}
+if (!!config.wordsBrightness) {
+  stylesToApply = [...stylesToApply, wordBrightnessStyle(config.wordsBrightness)]
+}
+if (!!config.blurOpacity) {
+  app.on('browser-window-blur', (event, browserWindow) =>
+    browserWindow.setOpacity(config.blurOpacity)
+  );
+  app.on('browser-window-focus', (event, browserWindow) =>
+    browserWindow.setOpacity(1.0)
+  );
+}
+
 app.on('ready', () => {
   const { workAreaSize } = screen.getPrimaryDisplay();
   const window = new BrowserWindow({
-    show:false,
+    show: false,
     frame: false,
     transparent: true,
-    // width: 980,
-    // height: 300,
     resizable: false,
     skipTaskbar: true,
-    icon: './mt-favicon.ico',
+    icon: './assets/mt-favicon.ico',
+    ...(!!config.windowConfig && config.windowConfig),
   });
-
-  window.loadURL('https://monkeytype.com');
-  window.webContents.openDevTools({ mode: 'detach' });
-
-  // window.on('show', () => {
-  //   window.setSize(workAreaSize.width, workAreaSize.height);
-  //   window.center();
-  // });
-
-  // window.on('hide', () => {
-  //   window.setSize(workAreaSize.width - 1, workAreaSize.height - 1);
-  // });
+  window.loadURL(monkeytypeURL);
+  if (!!config.showDevTools) {
+    window.webContents.openDevTools({ mode: 'detach' });
+  }
   window.webContents.on('ready-to-show', () => {
     // Set the transparent background
-    window.webContents.executeJavaScript(`
-      const bgColor = getComputedStyle(document.body).getPropertyValue('--bg-color');
-      document.body.style.backgroundColor = bgColor + '${backgroundOpacity}';
-    `);
+    if (!!config.backgroundOpacity) {
+     setBackgroundOpacity(window, config.backgroundOpacity);
+    }
+    if (!!config.draggable) {
+      makeKeyDownDraggableHandle(window, config.draggable.elemID, config.draggable.keyCode);
+    }
+
     // Delete Sections
     sectionsToRemove.forEach(removeElemById(window));
     // Apply Styles
     stylesToApply.forEach(applyStylesById(window));
     window.show();
   });
-});
-app.on ('browser-window-blur', function (event, browserWindow)
-{
-    browserWindow.setOpacity (0.1);
-});
-//
-app.on ('browser-window-focus', function (event, browserWindow)
-{
-    browserWindow.setOpacity (1.0);
 });
